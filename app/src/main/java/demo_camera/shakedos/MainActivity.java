e2d825226fa3ddbc092bf71a30ff43aa4cd4f43c
Shaked Klein Orbach <pigiman@gmail.com>
2015-08-27 00:19:17 +0300

    Load device settings
    emulator vs device recognition

--- app/src/main/java/demo_camera/shakedos/MainActivity.java
+++ app/src/main/java/demo_camera/shakedos/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Device.load();
         setContentView(R.layout.activity_main);
         wrapper = new Wrapper(this);
         picture = new Picture();
